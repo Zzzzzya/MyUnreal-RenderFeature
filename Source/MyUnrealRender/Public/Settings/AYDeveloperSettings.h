12832be911c4c69b260a3fe324d00dd3db025994
anyeezhao <anyeezhao@tencent.com>
2025-02-24 18:43:09 +0800

    Init Commit

---
+++ Source/MyUnrealRender/Public/Settings/AYDeveloperSettings.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "AYDeveloperSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUNREALRENDER_API UAYDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
